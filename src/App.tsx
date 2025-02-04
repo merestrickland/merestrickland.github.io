4d7c2db3740ac9b7073a32745c4488067d02f1b8
Meredith Strickland <mere.strickland@gmail.com>
2025-02-04 17:39:31 -0500

    initialize vite react

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+// import useMousePosition from "~/hooks/useMousePosition";
+
+// import EyeFollow from "~/components/EyeFollow";
+
+function App() {
+  // const mousePosition = useMousePosition();
+  return (
+    <div className="container">
+      <h1>Heyyyyyy</h1>
+      {/* <p>
+        Your cursor position:
+        <br />
+        {JSON.stringify(mousePosition)}
+      </p> */}
+      {/* <EyeFollow mousePosition={mousePosition} /> */}
+      {/* <marquee className={styles.banner}>
+        This site is a work in progress. Thank you for being here!
+      </marquee> */}
+    </div>
+  );
+}
+
+export default App;
